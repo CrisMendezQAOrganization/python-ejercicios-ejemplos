c27092ccdf75549c39c7ea871d22227a1f6d9ee9
CrisMendezQA <crismendezfotografia@gmail.com>
2026-06-04 20:41:23 +0200

    ejercicios 2

--- ejercicios/t3e1preciototal.py
+++ ejercicios/t3e1preciototal.py
@@ -1,29 +1,31 @@
-#Pedir datos a la usuaria
+#Pedir datos a la usuaria. Mostrar cantidad, nombre, descuento y precio total con descuento
 nombre_producto = input("Nombre del producto: ")
 precio_unidad = float(input("Precio por unidad: "))
-cantidad = int(input("Cantidad: "))
+cantidad = int(input("Cantidad a comprar: "))
 descuento = float(input("Descuento (%): "))
 
 #Cálculo del descuento
 def calcular_descuento(precio_unidad, cantidad, descuento):
     total = precio_unidad * cantidad
     descuento = total * (descuento / 100)
-    total_final = total - descuento
-    return total_final
+    total_condesc = total - descuento
+    return total_condesc
 
-#Mostrar cantidad, nombre y desuento con precio total con descuento
-nombre_producto = input("Nombre del producto: ")
-precio_unidad = float(input("Precio por unidad: "))
-cantidad = int(input("Cantidad a comprar: "))
-descuento = float(input("Descuento (%): "))
+total_condesc = calcular_descuento(precio_unidad, cantidad, descuento)
 
-print("Nombreroducto:", nombre_producto)
+print("Nombre del producto:", nombre_producto)
 print("Cantidad:", cantidad)
 print("Descuento:", descuento, "%")
-print("Precio total con descuento:", total_final)
+print("Precio total con descuento:", total_condesc)
 
 #Función que reciba una cantidad y le agregue 21%IVA
-def agregar_iva(total):
-    iva = total * 0,21
-    return total + iva
+def agregar_iva(total_condesc):
+    iva = total_condesc * 0.21
+    total_coniva = total_condesc + iva
+    return total_coniva
+    
 
+#Mostrar total con IVA aplicado
+total_coniva = agregar_iva(total_condesc)
+
+print("Total con IVA: ", total_coniva)
---
+++ ejercicios/t3e2conversor.py
@@ -0,0 +1,21 @@
+#Pedir una cantidad en euros
+euros = float(input("Cantidad en euros: "))
+
+#Función recibir euros y devolver en dólares (1 euros = 1.1 dólares)
+def euros_a_dolares(euros):
+    dolares = euros * 1.1
+    return dolares
+
+#Función recibir euros y devolver libras (1 euro = 0.87 libras)
+def euros_a_libras(euros):
+    libras = euros * 0.87
+    return libras
+
+#Muestra la cantidad en euros, dólares y libras
+dolares = euros_a_dolares(euros)
+libras = euros_a_libras(euros)
+
+print("Euros:", euros)
+print("Dólares: ", dolares)
+print("Libras: ", libras)
+    
