ece1cbfb5b7fe7e2866d3354585af95d457f8d2b
CrisMendezQA <crismendezfotografia@gmail.com>
2026-06-09 20:31:53 +0200

    Update t3e1preciototal.py

--- ejercicios/t3e1preciototal.py
+++ ejercicios/t3e1preciototal.py
@@ -1,31 +1,31 @@
+#FUNCIONES SIEMPRE PONER ARRIBA POR SI ACASO
+#Cálculo del descuento
+def calcular_descuento(precio_unidad, cantidad, descuento):
+    total = precio_unidad * cantidad
+    total_desc= (total * descuento) / 100
+    return total - total_desc
+    
+def agregar_iva(precio_unidad):
+    iva = precio_unidad * 0.21
+    return precio_unidad + iva
+
+
 #Pedir datos a la usuaria. Mostrar cantidad, nombre, descuento y precio total con descuento
 nombre_producto = input("Nombre del producto: ")
 precio_unidad = float(input("Precio por unidad: "))
 cantidad = int(input("Cantidad a comprar: "))
 descuento = float(input("Descuento (%): "))
 
-#Cálculo del descuento
-def calcular_descuento(precio_unidad, cantidad, descuento):
-    total = precio_unidad * cantidad
-    descuento = total * (descuento / 100)
-    total_condesc = total - descuento
-    return total_condesc
 
-total_condesc = calcular_descuento(precio_unidad, cantidad, descuento)
+precio_final = calcular_descuento(precio_unidad, cantidad, descuento)
 
 print("Nombre del producto:", nombre_producto)
 print("Cantidad:", cantidad)
 print("Descuento:", descuento, "%")
-print("Precio total con descuento:", total_condesc)
+print("Precio total con descuento:", precio_final)
 
-#Función que reciba una cantidad y le agregue 21%IVA
-def agregar_iva(total_condesc):
-    iva = total_condesc * 0.21
-    total_coniva = total_condesc + iva
-    return total_coniva
-    
+
 
 #Mostrar total con IVA aplicado
-total_coniva = agregar_iva(total_condesc)
-
+total_coniva = agregar_iva(precio_final)
 print("Total con IVA: ", total_coniva)
